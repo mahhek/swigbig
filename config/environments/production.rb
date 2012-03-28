1a22a023b93a8258a8f8d901fb025ee15b2b9e83
Usman <developer@developer-XPS-M1530.(none)>
2012-03-28 23:27:48 +0500

    changes for confirm email host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Swigbig::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => Rails.root.to_s }
+  config.action_mailer.default_url_options = { :host => 'cold-wind-2815.herokuapp.com' }
 end
 
 ActiveMerchant::Billing::Base.mode = :production
